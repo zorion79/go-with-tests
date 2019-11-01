499a2b9ea64f9bf031bd5602399aa444a06e6c77
Aleksandr Mogilnikov <zorion79@gmail.com>
2019-11-01 15:40:38 +1000

    fixup! added package maps

--- maps/maps.go
+++ maps/maps.go
@@ -1,1 +1,65 @@
 package maps
+
+const (
+	ErrWordExist        = DictionaryErr("word exists")
+	ErrWordDoesNotExist = DictionaryErr("word does not exist")
+	ErrNotFound         = DictionaryErr("could not find the word")
+)
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
+
+type Dictionary map[string]string
+
+func (d Dictionary) Search(word string) (string, error) {
+	result, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+	return result, nil
+}
+
+func (d Dictionary) Add(key string, value string) error {
+	_, err := d.Search(key)
+
+	switch err {
+	case ErrNotFound:
+		d[key] = value
+		return nil
+	case nil:
+		return ErrWordExist
+	default:
+		return err
+	}
+}
+
+func (d Dictionary) Update(word string, newDefinition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = newDefinition
+		return nil
+	default:
+		return err
+	}
+}
+
+func (d Dictionary) Delete(word string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		delete(d, word)
+		return nil
+	default:
+		return err
+	}
+}
--- maps/maps_test.go
+++ maps/maps_test.go
@@ -1,1 +1,95 @@
 package maps
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSearch(t *testing.T) {
+	dictionary := Dictionary{"test": "this is just a test"}
+
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
+
+		assert.Equal(t, want, got)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+
+		assert.Error(t, err)
+		assert.Equal(t, ErrNotFound, err)
+	})
+}
+
+func TestAdd(t *testing.T) {
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+
+		_ = dictionary.Add("test", "this is just a test")
+
+		want := "this is just a test"
+		got, err := dictionary.Search("test")
+
+		assert.NoError(t, err)
+		assert.Equal(t, want, got)
+	})
+
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+
+		err := dictionary.Add(word, "new test")
+		assert.Error(t, err)
+		assert.Equal(t, ErrWordExist, err)
+	})
+}
+
+func TestUpdate(t *testing.T) {
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+
+		newDefinition := "new definition"
+		err := dictionary.Update(word, newDefinition)
+		assert.NoError(t, err)
+
+		search, err := dictionary.Search(word)
+
+		assert.NoError(t, err)
+		assert.Equal(t, newDefinition, search)
+	})
+
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		definition := "new definition"
+
+		err := dictionary.Update("test", definition)
+
+		assert.Error(t, err)
+		assert.Equal(t, ErrWordDoesNotExist, err)
+	})
+}
+
+func TestDelete(t *testing.T) {
+	t.Run("word exists", func(t *testing.T) {
+		word := "test"
+		dictionary := Dictionary{word: "test definition"}
+
+		_ = dictionary.Delete(word)
+
+		_, err := dictionary.Search(word)
+		assert.Equal(t, ErrNotFound, err)
+	})
+
+	t.Run("word does not exist", func(t *testing.T) {
+		dictionary := Dictionary{}
+
+		err := dictionary.Delete("test")
+		assert.Equal(t, ErrWordDoesNotExist, err)
+	})
+}
